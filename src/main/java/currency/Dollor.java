633d62f476cf18f5294bb59a5fd7ed7ffc747514
homyeong.won <homyeong.won@nts-corp.com>
2021-03-08 18:21:56 +0900

    컴파일 에러 제거

---
+++ src/main/java/currency/Dollor.java
@@ -0,0 +1,11 @@
+package currency;
+
+public class Dollor {
+	int amount;
+
+	Dollor(int amount) {
+	}
+
+	void times(int multiplier) {
+	}
+}
--- src/test/java/currency/ApplicationTest.java
+++ src/test/java/currency/ApplicationTest.java
@@ -2,7 +2,11 @@ package currency;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest {
+
+	@Test
 	public void testMultiplication() {
 		Dollor five = new Dollor(5);
 		five.times(2);
