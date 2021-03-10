ff5cb8ad6153a9d2f19b3d3dbbbf3214d8751501
homyeong.won <homyeong.won@nts-corp.com>
2021-03-10 18:25:26 +0900

    Dollar 오타 수정

---
+++ src/main/java/currency/Dollar.java
@@ -0,0 +1,13 @@
+package currency;
+
+public class Dollar {
+	int amount;
+
+	Dollar(int amount) {
+		this.amount = amount;
+	}
+
+	Dollar times(int multiplier) {
+		return new Dollar(amount * multiplier);
+	}
+}
--- src/test/java/currency/ApplicationTest.java
+++ src/test/java/currency/ApplicationTest.java
@@ -8,8 +8,8 @@ class ApplicationTest {
 
 	@Test
 	public void testMultiplication() {
-		Dollor five = new Dollor(5);
-		Dollor product = five.times(2);
+		Dollar five = new Dollar(5);
+		Dollar product = five.times(2);
 		assertEquals(10, product.amount);
 		product = five.times(3);
 		assertEquals(15, product.amount);
