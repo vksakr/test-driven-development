c9fc9940b57d2f515a530afa4133f5516cd21252
homyeong.won <homyeong.won@nts-corp.com>
2021-03-10 18:31:33 +0900

    equals 비교 테스트 작성

--- src/test/java/currency/ApplicationTest.java
+++ src/test/java/currency/ApplicationTest.java
@@ -14,4 +14,9 @@ class ApplicationTest {
 		product = five.times(3);
 		assertEquals(15, product.amount);
 	}
+
+	@Test
+	public void testEquality() {
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+	}
 }
