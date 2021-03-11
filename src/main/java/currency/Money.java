e2e536b9df4bf010bf3b1bc1b7943b7e4c7dc1dc
homyeong.won <vksakr@gmail.com>
2021-03-12 02:55:42 +0900

    통화 테스트 통과를 위한 임시 구현

--- src/main/java/currency/Money.java
+++ src/main/java/currency/Money.java
@@ -14,6 +14,8 @@ public abstract class Money {
 
 	abstract Money times(int multiplier);
 
+	abstract String currency();
+
 	public boolean equals(Object object) {
 		Money money = (Money)object;
 		return amount == money.amount
