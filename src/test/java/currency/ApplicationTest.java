fd0d65da2ce7d2eedfbcd3ff5eb22357c24ba2ba
homyeong.won <vksakr@gmail.com>
2021-03-12 01:47:45 +0900

    불필요한 임시변수 제거

--- src/test/java/currency/ApplicationTest.java
+++ src/test/java/currency/ApplicationTest.java
@@ -9,9 +9,7 @@ class ApplicationTest {
 	@Test
 	public void testMultiplication() {
 		Dollar five = new Dollar(5);
-		Dollar product = five.times(2);
 		assertEquals(new Dollar(10), five.times(2));
-		product = five.times(3);
 		assertEquals(new Dollar(15), five.times(3));
 	}
 
