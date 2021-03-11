ca191874bc7daad145f935f6fc6a8254f43873e2
homyeong.won <vksakr@gmail.com>
2021-03-12 02:24:41 +0900

    Money 에 Dollar 를 반환하는 팩토리 메서드 도입

--- src/main/java/currency/Money.java
+++ src/main/java/currency/Money.java
@@ -4,6 +4,10 @@ public class Money {
 
 	protected int amount;
 
+	public static Dollar dollar(int amount) {
+		return new Dollar(amount);
+	}
+
 	public boolean equals(Object object) {
 		Money money = (Money)object;
 		return amount == money.amount
