e0f461c79c874c53c14bbaec279d2e33ca70558b
homyeong.won <vksakr@gmail.com>
2021-03-12 02:43:33 +0900

    franc 도 팩토리 메서드 패턴 적용

--- src/main/java/currency/Money.java
+++ src/main/java/currency/Money.java
@@ -8,6 +8,10 @@ public abstract class Money {
 		return new Dollar(amount);
 	}
 
+	public static Money franc(int amount) {
+		return new Franc(amount);
+	}
+
 	abstract Money times(int multiplier);
 
 	public boolean equals(Object object) {
