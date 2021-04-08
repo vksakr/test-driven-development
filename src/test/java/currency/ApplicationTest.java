ae40a36b1d30db39b58659bb3b3db2abb2bb7a0d
homyeong.won <vksakr@gmail.com>
2021-04-09 00:17:52 +0900

    Bank.reduce 에 Money 가 인자로 주어지는 테스트

--- src/test/java/currency/ApplicationTest.java
+++ src/test/java/currency/ApplicationTest.java
@@ -39,7 +39,7 @@ class ApplicationTest {
 	public void testPlusReturnsSum() {
 		Money five = Money.dollar(5);
 		Expression result = five.plus(five);
-		Sum sum = (Sum) result;
+		Sum sum = (Sum)result;
 		assertEquals(five, sum.augend);
 		assertEquals(five, sum.addend);
 	}
@@ -50,5 +50,12 @@ class ApplicationTest {
 		Bank bank = new Bank();
 		Money result = bank.reduce(sum, "USD");
 		assertEquals(Money.dollar(7), result);
+	}
+
+	@Test
+	public void testReduceMoney() {
+		Bank bank = new Bank();
+		Money result = bank.reduce(Money.dollar(1), "USD");
+		assertEquals(Money.dollar(1), result);
 	}
 }
