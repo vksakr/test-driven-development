5f2be1309c3c4cb6fc7d62f461d567d81164f78d
homyeong.won <homyeong.won@nts-corp.com>
2021-04-12 17:41:23 +0900

    Pair 를 Bank 의 inner class 로 옮김

--- src/main/java/currency/Bank.java
+++ src/main/java/currency/Bank.java
@@ -3,6 +3,25 @@ package currency;
 import java.util.Hashtable;
 
 public class Bank {
+	private class Pair {
+		private String from;
+		private String to;
+
+		Pair(String from, String to) {
+			this.from = from;
+			this.to = to;
+		}
+
+		public boolean equals(Object object) {
+			Pair pair = (Pair)object;
+			return from.equals(pair.from) && to.equals(pair.to);
+		}
+
+		public int hashCode() {
+			return 0;
+		}
+	}
+
 	private Hashtable<Pair, Integer> rates = new Hashtable<>();
 
 	Money reduce(Expression source, String to) {
